e1174b03e7b575688c7275b48e9bc7a2cca42721
Dan <danlowry1991@googlemail.com>
2023-03-04 13:48:17 +0000

    jupyter

---
+++ run_jupyter.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d I:\
+cd "I:\dev\pricing_interest_rate_derivatives"
+"I:\dev\pricing_interest_rate_derivatives\venv\Scripts\jupyter-notebook.exe" 
---
+++ run_jupyter.sh
@@ -0,0 +1,2 @@
+cd /home/dan/coding_projects/dan/energy/Notebooks
+"../venv/bin/jupyter-notebook" 
